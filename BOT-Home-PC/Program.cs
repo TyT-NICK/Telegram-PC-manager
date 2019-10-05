5af26424bf26d0c3b31ac50c57a8632226f441af
Artem <53619450+TyT-NICK@users.noreply.github.com>
2019-10-05 19:51:42 +0800

    Minor Fix

--- BOT-Home-PC/Program.cs
+++ BOT-Home-PC/Program.cs
@@ -6,9 +6,9 @@ namespace BOT_Home_PC
 {
     class Program
     {
-        //835536006:AAHR2L3YYmFWdnLcrTrZBQOhKtu9sn2Pfe4
+        
         static WebProxy proxy = new WebProxy("14.188.41.69", 8080);
-        static TelegramBotAdapted bot = new TelegramBotAdapted("835536006:AAHR2L3YYmFWdnLcrTrZBQOhKtu9sn2Pfe4", proxy);
+        static TelegramBotAdapted bot = new TelegramBotAdapted("", proxy);
         static void Main(string[] args)
         {
             bot.OnMessage += BotOnMessageRecieved;
