b511a58587b9a4332953b5ed54740cb53867ac6b
Artem <53619450+TyT-NICK@users.noreply.github.com>
2019-10-05 19:49:05 +0800

    First upload

---
+++ BOT-Home-PC/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Telegram.Bot;
+using System.Net;
+
+namespace BOT_Home_PC
+{
+    class Program
+    {
+        //835536006:AAHR2L3YYmFWdnLcrTrZBQOhKtu9sn2Pfe4
+        static WebProxy proxy = new WebProxy("14.188.41.69", 8080);
+        static TelegramBotAdapted bot = new TelegramBotAdapted("835536006:AAHR2L3YYmFWdnLcrTrZBQOhKtu9sn2Pfe4", proxy);
+        static void Main(string[] args)
+        {
+            bot.OnMessage += BotOnMessageRecieved;
+            bot.StartReceiving();
+            while (Console.ReadKey().Key != ConsoleKey.PageDown) { }
+            bot.StopReceiving();
+        }
+
+        static void BotOnMessageRecieved(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ BOT-Home-PC/TelegramBotAdapted.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Telegram.Bot;
+
+namespace BOT_Home_PC
+{
+    class TelegramBotAdapted : TelegramBotClient
+    {
+        /*
+         * 
+         * static WebProxy proxy = new WebProxy("14.188.41.69", 8080);
+        static TelegramBotClient bot = new TelegramBotClient("694238993:AAHJZVyK6IvQ2lFxyuH-vepfEuqpBWqk5WA", proxy);
+         * */
+        public TelegramBotAdapted(string token, System.Net.WebProxy proxy) : base(token, proxy) { }
+    }
+}
