2bd5949024160a6d3983b4f9108423bbc2cd2b18
Artem <53619450+TyT-NICK@users.noreply.github.com>
2019-10-06 21:46:22 +0800

    Validation process

--- BOT-Home-PC/Program.cs
+++ BOT-Home-PC/Program.cs
@@ -1,25 +1,124 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
 using Telegram.Bot;
-using System.Net;
 
 namespace BOT_Home_PC
 {
     class Program
     {
-        
-        static WebProxy proxy = new WebProxy("14.188.41.69", 8080);
-        static TelegramBotAdapted bot = new TelegramBotAdapted("", proxy);
+        static TelegramBotAdapted bot;
         static void Main(string[] args)
         {
-            bot.OnMessage += BotOnMessageRecieved;
-            bot.StartReceiving();
-            while (Console.ReadKey().Key != ConsoleKey.PageDown) { }
-            bot.StopReceiving();
+            bot = InitializeBot();
+            if (bot != null)
+            {
+                bot.StartReceiving();
+                while (Console.ReadKey().Key != ConsoleKey.PageDown) { }
+                bot.StartReceiving();
+            }
+            else
+                Console.WriteLine("Bot is not created");
         }
 
-        static void BotOnMessageRecieved(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        //TODO: Разнсти инициализацию нового и существующего бота в разные методы
+        public static TelegramBotAdapted InitializeBot()
         {
+            TelegramBotAdapted bot = null;
 
+            if (!Directory.Exists(@"bot"))
+                Directory.CreateDirectory(@"bot");
+
+            string pathToken = @"bot/bot-info.tbi";
+            string pathUserList = @"bot/users.ul";
+            string userInput = "";
+
+            string token = "";
+            string password = "";
+
+            if (!File.Exists(pathToken))
+            {
+                while (bot == null)
+                {
+                    try
+                    {
+                        Console.WriteLine("Enter bot token (/cancel to exit): ");
+                        userInput = Console.ReadLine();
+                        if (userInput == "/cancel")
+                            return null;
+                        token = userInput;
+
+                        Console.WriteLine("Enter bot password (/cancel to exit): ");
+                        userInput = Console.ReadLine();
+                        if (userInput == "/cancel")
+                            return null;
+                        password = userInput;
+                        userInput = "";
+                        while (userInput != password)
+                        {
+                            Console.WriteLine("Once more: ");
+                            userInput = Console.ReadLine();
+                            if (userInput == "/cancel")
+                                return null;
+                            else if (userInput != password)
+                                Console.WriteLine("Entered passwords differ");
+                        }
+
+                        bot = new TelegramBotAdapted(token, password, new List<int>()) ;
+                    }
+                    catch (ArgumentException ex)
+                    {
+                        Debug.WriteLine(ex.Message);
+                        Console.WriteLine("The token is incorrect");
+                    }
+                }
+                using (BinaryWriter writer = new BinaryWriter(File.Open(pathToken, FileMode.Create)))
+                {
+                    writer.Write(token);
+                    writer.Write(password);
+                }
+            }
+            else
+            {
+                List<int> userList = new List<int>();
+
+                using (BinaryReader reader = new BinaryReader(File.Open(pathToken, FileMode.Open)))
+                {
+                    token = reader.ReadString();
+                    password = reader.ReadString();
+                }
+                using (BinaryReader reader = new BinaryReader(File.Open(pathUserList, FileMode.Open)))
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            userList.Add(reader.ReadInt32());
+                        }
+                        catch
+                        {
+                            break;
+                        }
+                    }
+                }
+                bot = new TelegramBotAdapted(token, password, userList);
+            }
+            Console.Clear();
+
+            Console.WriteLine("Bot initialized");
+            return bot;
+        }
+
+        public static void WriteUserList(List<int> userList)
+        {
+            string fileName = @"bot/users.ul";
+
+            using (BinaryWriter writer = new BinaryWriter(File.Create(fileName)))
+            {
+                foreach (var e in userList)
+                    writer.Write(e);
+            }
         }
     }
 }
--- BOT-Home-PC/TelegramBotAdapted.cs
+++ BOT-Home-PC/TelegramBotAdapted.cs
@@ -2,16 +2,73 @@
 using System.Collections.Generic;
 using System.Text;
 using Telegram.Bot;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
 
 namespace BOT_Home_PC
 {
     class TelegramBotAdapted : TelegramBotClient
     {
-        /*
-         * 
-         * static WebProxy proxy = new WebProxy("14.188.41.69", 8080);
-        static TelegramBotClient bot = new TelegramBotClient("694238993:AAHJZVyK6IvQ2lFxyuH-vepfEuqpBWqk5WA", proxy);
-         * */
-        public TelegramBotAdapted(string token, System.Net.WebProxy proxy) : base(token, proxy) { }
+        //public static Dictionary validatedUserList
+
+        public static WebProxy currentProxy = new WebProxy("14.188.41.69", 8080);
+
+        private string Password { get; set; }
+        private List<int> ValidatedUsers { get; set; }
+        private Dictionary<string, string> HelpList { get; set; }
+
+        public TelegramBotAdapted(string token, string password, List<int> userList) : base(token, currentProxy) 
+        {
+            this.Password = password;
+            this.ValidatedUsers = userList;
+            
+            this.OnMessage += BotMessageRecieved;
+
+        }
+
+        private void BotMessageRecieved(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            Debug.WriteLine($"{e.Message.From.FirstName} {e.Message.From.LastName} ({e.Message.From.Id}): [{e.Message.Type.ToString()}] {e.Message.Text}");
+            
+            //TODO: Обработка нетекстового сообщения
+            switch (e.Message.Text.Trim())
+            {
+                case "/start":
+                    if (ValidatedUsers.Contains(e.Message.From.Id))
+                    { 
+                        this.SendTextMessageAsync(e.Message.Chat.Id, "You are already authorized");
+                    }
+                    else
+                    {
+                        this.SendTextMessageAsync(e.Message.Chat.Id, "New user! Enter password to continue");
+                        this.OnMessage -= BotMessageRecieved;
+                        this.OnMessage += ValidationProcess;
+                    }
+                    break;
+                case "/testcommand":
+                    this.SendTextMessageAsync(e.Message.Chat.Id, "/testcommand");
+                    break;
+                case "/gettestfile":
+                    this.SendDocumentAsync(e.Message.Chat.Id, File.Open("self.jpg", FileMode.Open));
+                    break;
+            }
+        }
+
+        private void ValidationProcess(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            if (e.Message.Text == this.Password)
+            {
+                this.SendTextMessageAsync(e.Message.Chat.Id, "Correct Password");
+                this.ValidatedUsers.Add(e.Message.From.Id);
+                this.OnMessage -= ValidationProcess;
+                this.OnMessage += BotMessageRecieved;
+                Program.WriteUserList(this.ValidatedUsers);
+            }
+            else
+            {
+                this.SendTextMessageAsync(e.Message.Chat.Id, "Wrong Password. Access denied");
+            }
+        }
     }
 }
